6368602f67926721551ab182a23114274793c5c5
Shrutika-khade <shrutikakhade2020@gmail.com>
2026-06-18 16:53:26 +0530

    Updated SecurityConfig for Render

--- Complaint-Managment-System/backend/complaint/src/main/java/com/project/complaint/security/SecurityConfig.java
+++ Complaint-Managment-System/backend/complaint/src/main/java/com/project/complaint/security/SecurityConfig.java
@@ -32,6 +32,12 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         .authorizeHttpRequests(auth -> auth
 
+               .requestMatchers(
+                        "/",
+                        "/error",
+                        "/favicon.ico"
+                  ).permitAll()
+
                 // Public APIs
                 .requestMatchers(HttpMethod.POST,
 
@@ -66,7 +72,13 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
     org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
 
-    configuration.setAllowedOrigins(java.util.List.of("http://127.0.0.1:5500", "http://localhost:5500"));
+  configuration.setAllowedOrigins(
+    java.util.List.of(
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "https://smart-complaint-management-system-8qyp.onrender.com"
+    )
+);
     configuration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(java.util.List.of("*"));
     configuration.setAllowCredentials(true);
